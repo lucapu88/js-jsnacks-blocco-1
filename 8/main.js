42161e3cd4ee6d3ce95a683dae044ccb23074399
lucapu88 <caputoluca88@gmail.com>
2019-11-20 18:21:57 +0100

    finalmente risolto!

--- 8/main.js
+++ 8/main.js
@@ -1,10 +1,21 @@
 // Chiedi un numero di 4 cifre all’utente e calcola la somma
 // di tutte le cifre che compongono il numero.
 var numero = prompt('digita un numero di 4 cifre');
-var numeri = [];
-numeri.push(numero);
-console.log(numeri);
-numero = numero.split('');
-console.log(numero);
-numeri.sum();
-console.log(numeri);
+
+somma = 0;
+for (var i = 0; i < numero.length; i++) {
+  somma = parseInt(somma) + parseInt(numero[i]);
+  
+}
+  console.log(somma);
+// var numeri = [];
+// numeri.push(numero);
+// console.log(numeri);
+// numero = numero.split('');
+// console.log(numero);
+//
+//
+// function myFunc(total, num) {
+//   return total + num;
+// }
+// console.log(numeri.reduce(myFunc));
